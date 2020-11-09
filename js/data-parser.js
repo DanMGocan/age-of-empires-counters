c63317448f17decddde9df7aa1c1f64a7259e058
Dan Gocan <dangocan@outlook.com>
2020-11-09 14:51:42 +0000

    Working on the JSON parsing

--- js/data-parser.js
+++ js/data-parser.js
@@ -1,3 +1,5 @@
 /* Script used to import all the JSON data files and have them ready
 as modules, to be exported as required by the main app. */
 
+import information from "landing-page.js";
+console.log(information);
--- js/landing-page.js
+++ js/landing-page.js
@@ -1,1 +1,14 @@
-$(".nav-container").load("./common/navbar.html");
+// $(".nav-container").load("./common/navbar.html");
+
+let information = "";
+
+const fs = require("fs");
+fs.readFile("../data/units.json", "utf-8", (err, data) => {
+    if(err) {
+        console.log("error"+ err);
+    } else {
+        let units = JSON.parse(data);
+        console.log(units[3]);
+    }
+})
+
